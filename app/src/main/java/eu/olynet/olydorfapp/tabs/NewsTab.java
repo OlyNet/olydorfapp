45d59a567e28d78429b4e5f90be8698cf4087f23
Martin Herrmann <martin.herrmann@olynet.eu>
2016-02-27 13:00:10 +0100

    Implemented swipe to refresh for the news tab.

--- app/src/main/java/eu/olynet/olydorfapp/tabs/NewsTab.java
+++ app/src/main/java/eu/olynet/olydorfapp/tabs/NewsTab.java
@@ -11,12 +11,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -33,7 +35,9 @@ import eu.olynet.olydorfapp.resources.ResourceManager;
 /**
  * @author <a href="mailto:simon.domke@olynet.eu">Simon Domke</a>
  */
-public class NewsTab extends Fragment {
+public class NewsTab extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    private SwipeRefreshLayout mRefreshLayout;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -42,46 +46,76 @@ public class NewsTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_news, container, false);
+
+        /* initiate SwipeRefreshLayout */
+        mRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.news_refresh_layout);
+        mRefreshLayout.setOnRefreshListener(this);
+
+        /* initiate RecycleView */
         mRecyclerView = (RecyclerView) v.findViewById(R.id.news_card_list);
+
         return v;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        final Context context = getContext();
 
-        (new AsyncTask<Void, Void, List<AbstractMetaItem<?>>>() {
+        new NewsUpdateTask().execute();
+    }
 
-            @Override
-            protected List<AbstractMetaItem<?>> doInBackground(Void... params) {
-                ResourceManager rm = ResourceManager.getInstance();
+    @Override
+    public void onRefresh() {
+        /* disable swipe to refresh while already refreshing */
+        mRefreshLayout.setEnabled(false);
+
+        // TODO: implement refreshing
+
+        /* do not call this from this function directly but from the NewsUpdateTask */
+        onLoadCompleted();
+    }
+
+    private void onLoadCompleted() {
+        // TODO: update the adapter and notify data set changed
+
+        /* disable refreshing animation and enable swipe to refresh again */
+        mRefreshLayout.setRefreshing(false);
+        mRefreshLayout.setEnabled(true);
+
+    }
+
+    protected class NewsUpdateTask extends AsyncTask<Void, Void, List<AbstractMetaItem<?>>> {
+
+        @Override
+        protected List<AbstractMetaItem<?>> doInBackground(Void... params) {
+            ResourceManager rm = ResourceManager.getInstance();
 
                 /* querying the ResourceManager for the needed data */
-                TreeSet<AbstractMetaItem<?>> tree = rm.getTreeOfMetaItems(NewsMetaItem.class);
+            TreeSet<AbstractMetaItem<?>> tree = rm.getTreeOfMetaItems(NewsMetaItem.class);
 
-                List<Integer> ids = new ArrayList<>();
-                for(AbstractMetaItem<?> item : tree) {
-                    ids.add(item.getId());
-                }
+            List<Integer> ids = new ArrayList<>();
+            for(AbstractMetaItem<?> item : tree) {
+                ids.add(item.getId());
+            }
 
                 /* requesting and returning the result array */
-                return rm.getItems(NewsMetaItem.class, ids,
-                        new AbstractMetaItem.DateDescComparator());
-            }
+            return rm.getItems(NewsMetaItem.class, ids,
+                    new AbstractMetaItem.DateDescComparator());
+        }
 
-            @Override
-            protected void onPostExecute(List<AbstractMetaItem<?>> result) {
-                super.onPostExecute(result);
+        @Override
+        protected void onPostExecute(List<AbstractMetaItem<?>> result) {
+            super.onPostExecute(result);
 
-                mAdapter = new NewsDataAdapter(context, result);
+            mAdapter = new NewsDataAdapter(getContext(), result);
 
-                mLayoutManager = new LinearLayoutManager(getActivity());
-                mRecyclerView.setHasFixedSize(true);
-                mRecyclerView.setAdapter(mAdapter);
-                mRecyclerView.setLayoutManager(mLayoutManager);
-                mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-            }
-        }).execute();
+            mLayoutManager = new LinearLayoutManager(getActivity());
+            mRecyclerView.setHasFixedSize(true);
+            mRecyclerView.setAdapter(mAdapter);
+            mRecyclerView.setLayoutManager(mLayoutManager);
+            mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+            onLoadCompleted();
+        }
     }
 }
