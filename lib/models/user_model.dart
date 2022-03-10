6665b6be2863a8fdce26246a4db2b383cb570505
Joscha von Andrian <joschavonandrian@gmail.com>
2022-03-10 21:05:00 +0100

    add user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,26 @@
+class AppUser {
+  final String id;
+  final String email;
+  final String name;
+  AppUser({
+    required this.id,
+    required this.email,
+    required this.name,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+    };
+  }
+
+  factory AppUser.fromMap(Map<String, dynamic> map) {
+    return AppUser(
+      id: map['id'],
+      name: map['name'],
+      email: map['email'],
+    );
+  }
+}
