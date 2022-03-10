adfe27af513c6a7c916e0c1fc6a202b81ffa7dd4
Joscha von Andrian <joschavonandrian@gmail.com>
2022-03-10 20:41:22 +0100

    update collection id

---
+++ lib/global/consts.dart
@@ -0,0 +1,10 @@
+class AppwriteConfig {
+  static const endpointUrl = "http://appwrite.960.eu:5500/v1";
+  static const projectId = "61f3034d10e3bbe97441";
+}
+
+class Routes {
+  static const login = "/login";
+  static const signUp = "/signUp";
+  static const bottomNavigationBar = "/bottomNavigationBar";
+}
--- lib/providers/events_provider.dart
+++ lib/providers/events_provider.dart
@@ -1,14 +1,14 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:olydorf/global/consts.dart';
 import 'package:olydorf/models/event_model.dart';
 import 'package:appwrite/appwrite.dart';
 
 final eventsListProvider = FutureProvider<List<Event>>((ref) async {
   try {
-    Client client = Client(endPoint: 'http://appwrite.960.eu:5500/v1');
-    client.setProject('61f3034d10e3bbe97441').setSelfSigned();
+    Client client = Client(endPoint: AppwriteConfig.endpointUrl);
+    client.setProject(AppwriteConfig.projectId).setSelfSigned();
     Database database = Database(client);
-    final res =
-        await database.listDocuments(collectionId: '61f329309b6e2c4d7954');
+    final res = await database.listDocuments(collectionId: 'events');
 
     return res.documents.map((doc) => Event.fromMap(doc.data)).toList();
   } on AppwriteException catch (e) {
