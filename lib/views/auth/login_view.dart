d05b72f4e72272f4d8698c098b88aaaaf14ac15d
Joscha von Andrian <joschavonandrian@gmail.com>
2022-03-11 12:37:59 +0100

    fix login form

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -21,47 +21,48 @@ class LoginView extends HookConsumerWidget {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 0),
       body: Form(
+          key: _formKey,
           child: Column(
-        children: [
-          TextFormField(
-            controller: _emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(hintText: "Email"),
-            validator: (text) {
-              if (text!.isEmpty || !text.contains('@')) {
-                return "Invalid email";
-              }
-            },
-          ),
-          TextFormField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(hintText: "Password"),
-            validator: (text) {
-              if (text!.isEmpty || text.length < 8) {
-                return "Passwort too short";
-              }
-            },
-          ),
-          _loading
-              ? const Center(child: CircularProgressIndicator())
-              : ElevatedButton(
-                  onPressed: (() async {
-                    if (!_formKey.currentState!.validate()) {
-                      return;
-                    }
-                    _loading = true;
-                    await auth.login(_emailController.text,
-                        _passwordController.text, context);
-                    _loading = false;
-                  }),
-                  child: const Text("login")),
-          TextButton(
-              onPressed: () =>
-                  Navigator.of(context).pushReplacementNamed(Routes.signUp),
-              child: const Text('sign up'))
-        ],
-      )),
+            children: [
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(hintText: "Email"),
+                validator: (text) {
+                  if (text!.isEmpty || !text.contains('@')) {
+                    return "Invalid email";
+                  }
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(hintText: "Password"),
+                validator: (text) {
+                  if (text!.isEmpty || text.length < 8) {
+                    return "Passwort too short";
+                  }
+                },
+              ),
+              _loading
+                  ? const Center(child: CircularProgressIndicator())
+                  : ElevatedButton(
+                      onPressed: (() async {
+                        if (!_formKey.currentState!.validate()) {
+                          return;
+                        }
+                        _loading = true;
+                        await auth.login(_emailController.text,
+                            _passwordController.text, context);
+                        _loading = false;
+                      }),
+                      child: const Text("login")),
+              TextButton(
+                  onPressed: () =>
+                      Navigator.of(context).pushReplacementNamed(Routes.signUp),
+                  child: const Text('sign up'))
+            ],
+          )),
     );
   }
 }
