8f8400c3ddc272c0985d1595a5f3624c603da52c
Joscha von Andrian <joschavonandrian@gmail.com>
2022-03-11 19:05:39 +0100

    improve design

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -17,55 +17,87 @@ class LoginView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(toolbarHeight: 0),
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
       body: Form(
           key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(hintText: "Email"),
-                validator: (text) {
-                  if (text!.isEmpty || !text.contains('@')) {
-                    return "Invalid email";
-                  }
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(hintText: "Password"),
-                validator: (text) {
-                  if (text!.isEmpty || text.length < 8) {
-                    return "Passwort too short";
-                  }
-                },
-              ),
-              _loading
-                  ? const Center(child: CircularProgressIndicator())
-                  : ElevatedButton(
-                      onPressed: (() async {
-                        if (!_formKey.currentState!.validate()) {
-                          return;
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(38.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.account_circle,
+                      size: 72,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        hintText: "example@email.com",
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (text) {
+                        if (text!.isEmpty || !text.contains('@')) {
+                          return "Invalid email";
                         }
-                        _loading = true;
-                        await ref.read(authProvider.notifier).login(
-                            _emailController.text,
-                            _passwordController.text,
-                            context);
-                        _loading = false;
-                      }),
-                      child: const Text("login")),
-              ElevatedButton(
-                  onPressed: () =>
-                      ref.read(authProvider.notifier).loginGoogle(context),
-                  child: const Text("login google")),
-              TextButton(
-                  onPressed: () =>
-                      Navigator.of(context).pushReplacementNamed(Routes.signUp),
-                  child: const Text('sign up'))
-            ],
+                      },
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: "Password",
+                        hintText: "********",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (text) {
+                        if (text!.isEmpty || text.length < 8) {
+                          return "Passwort too short";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    _loading
+                        ? const Center(child: CircularProgressIndicator())
+                        : ElevatedButton(
+                            onPressed: (() async {
+                              if (!_formKey.currentState!.validate()) {
+                                return;
+                              }
+                              _loading = true;
+                              await ref.read(authProvider.notifier).login(
+                                  _emailController.text,
+                                  _passwordController.text,
+                                  context);
+                              _loading = false;
+                            }),
+                            child: const Text("Login")),
+                    ElevatedButton(
+                        onPressed: () => ref
+                            .read(authProvider.notifier)
+                            .loginGoogle(context),
+                        child: const Text("Login with Google")),
+                    TextButton(
+                        onPressed: () => Navigator.of(context)
+                            .pushReplacementNamed(Routes.signUp),
+                        child: const Text('Sign up'))
+                  ],
+                ),
+              ),
+            ),
           )),
     );
   }
--- lib/views/auth/sign_up_view.dart
+++ lib/views/auth/sign_up_view.dart
@@ -17,71 +17,115 @@ class SignUpView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(toolbarHeight: 0),
+      appBar: AppBar(
+        title: const Text("Sign up"),
+      ),
       body: Form(
           key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(hintText: "Email"),
-                validator: (text) {
-                  if (text!.isEmpty || !text.contains('@')) {
-                    return "Invalid email";
-                  }
-                },
-              ),
-              TextFormField(
-                controller: _nameController,
-                decoration: const InputDecoration(hintText: "name"),
-                validator: (text) {
-                  if (text!.isEmpty || text.length < 3) {
-                    return "name too short (min 3)";
-                  }
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(hintText: "Password"),
-                validator: (text) {
-                  if (text!.isEmpty || text.length < 8) {
-                    return "Passwort too short (min 8)";
-                  }
-                },
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(hintText: "Password"),
-                validator: (text) {
-                  if (text != _passwordController.text) {
-                    return "Passwords don't match";
-                  }
-                },
-              ),
-              _loading
-                  ? const Center(child: CircularProgressIndicator())
-                  : ElevatedButton(
-                      onPressed: (() async {
-                        if (!_formKey.currentState!.validate()) {
-                          return;
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(38.0),
+                child: Column(
+                  children: [
+                    const Icon(
+                      Icons.account_circle,
+                      size: 72,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        hintText: "example@email.com",
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (text) {
+                        if (text!.isEmpty || !text.contains('@')) {
+                          return "Invalid email";
                         }
-                        _loading = true;
-                        await ref.read(authProvider.notifier).signUp(
-                              _emailController.text,
-                              _passwordController.text,
-                              _nameController.text,
-                              context,
-                            );
-                        _loading = false;
-                      }),
-                      child: const Text("sign up")),
-              TextButton(
-                  onPressed: () =>
-                      Navigator.of(context).pushReplacementNamed(Routes.login),
-                  child: const Text('login'))
-            ],
+                      },
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        hintText: "Your Name",
+                        labelText: "Name",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (text) {
+                        if (text!.isEmpty || text.length < 3) {
+                          return "name too short (min 3)";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: "Password",
+                        hintText: "********",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (text) {
+                        if (text!.isEmpty || text.length < 8) {
+                          return "Passwort too short (min 8)";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: "Repeat password",
+                        hintText: "********",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (text) {
+                        if (text != _passwordController.text) {
+                          return "Passwords don't match";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    _loading
+                        ? const Center(child: CircularProgressIndicator())
+                        : ElevatedButton(
+                            onPressed: (() async {
+                              if (!_formKey.currentState!.validate()) {
+                                return;
+                              }
+                              _loading = true;
+                              await ref.read(authProvider.notifier).signUp(
+                                    _emailController.text,
+                                    _passwordController.text,
+                                    _nameController.text,
+                                    context,
+                                  );
+                              _loading = false;
+                            }),
+                            child: const Text("Sign up")),
+                    TextButton(
+                        onPressed: () => Navigator.of(context)
+                            .pushReplacementNamed(Routes.login),
+                        child: const Text('Login'))
+                  ],
+                ),
+              ),
+            ),
           )),
     );
   }
