25ae17acbfea3d9c431daeb492e1a0ad142290bf
Joscha von Andrian <joschavonandrian@gmail.com>
2022-03-13 16:56:58 +0100

    add convert function

--- scripts/geojson2dart.py
+++ scripts/geojson2dart.py
@@ -1,3 +1,4 @@
+import json
 
 
 def polygon_center(polygon: list) -> tuple[float, float]:
@@ -10,7 +11,7 @@ def polygon_center(polygon: list) -> tuple[float, float]:
     return center_lat, center_lng
 
 
-if __name__ == "__main__":
+def test_polygon_center():
     bungalow = [
         [48.179139, 11.5538915],
         [48.1791103, 11.5538916],
@@ -20,3 +21,46 @@ if __name__ == "__main__":
     ]
 
     print(polygon_center(bungalow))
+
+
+start_str = """
+import 'package:latlong2/latlong.dart';
+
+class Building {
+  String label;
+  List<LatLng> points;
+  Building({required this.label, required this.points});
+}
+
+List map_data = [
+"""
+end_str = "];"
+
+
+def geojson2dart():
+    with open('map_data.dart', 'w') as out_file:
+        out_file.write(start_str)
+        with open('map_data/olydorf.geojson', 'r') as file:
+            data = json.load(file)
+            for building in data['features']:
+                if "addr:unit" in building['properties']:
+
+                    coords = building['geometry']['coordinates'][0]
+                    if len(coords) == 5:
+                        building_start_str = f"""
+                        Building(
+                            label: "{building['properties']["addr:unit"]}",
+                            points: [
+                        """
+                        out_file.write(building_start_str)
+
+                        for i in range(4):
+                            out_file.write(
+                                f"LatLng({coords[i][1]}, {coords[i][0]}),")
+
+                        out_file.write("],),")
+        out_file.write(end_str)
+
+
+if __name__ == "__main__":
+    geojson2dart()
