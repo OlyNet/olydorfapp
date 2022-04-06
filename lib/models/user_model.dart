97da810f6b06060b52a66124fc2871adb0c2432b
Joscha von Andrian <joschavonandrian@gmail.com>
2022-04-07 00:23:59 +0200

    add address

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,10 +2,13 @@ class AppUser {
   final String id;
   final String email;
   final String name;
+  final String address;
+
   AppUser({
     required this.id,
     required this.email,
     required this.name,
+    this.address = "",
   });
 
   Map<String, dynamic> toMap() {
@@ -13,14 +16,15 @@ class AppUser {
       'id': id,
       'email': email,
       'name': name,
+      'address': address,
     };
   }
 
   factory AppUser.fromMap(Map<String, dynamic> map) {
     return AppUser(
-      id: map['id'],
-      name: map['name'],
-      email: map['email'],
-    );
+        id: map['id'],
+        name: map['name'],
+        email: map['email'],
+        address: map['address'] ?? "");
   }
 }
