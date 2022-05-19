de477012ed0973b9106a674d86dcec85756ea535
Joscha von Andrian <joschavonandrian@gmail.com>
2022-05-19 13:37:36 +0200

    show update error message

--- lib/views/profile/edit_profile_view.dart
+++ lib/views/profile/edit_profile_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:olydorf/api/profile_helper.dart';
+import 'package:olydorf/models/user_model.dart';
+import 'package:olydorf/providers/auth_provider.dart';
 
 class EditProfileView extends HookConsumerWidget {
   EditProfileView({Key? key}) : super(key: key);
@@ -9,6 +11,8 @@ class EditProfileView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final AppUser? currentUser = ref.watch(authProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Edit profile"),
@@ -16,6 +20,8 @@ class EditProfileView extends HookConsumerWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(children: [
+          const Text("home:"),
+          Text(currentUser != null ? currentUser.address : ""),
           TextFormField(
             controller: bungalowController,
             decoration: const InputDecoration(
@@ -25,11 +31,21 @@ class EditProfileView extends HookConsumerWidget {
             ),
           ),
           ElevatedButton(
-              onPressed: () => ProfileHelper.saveBungalow(
-                    ref,
-                    bungalowController
-                        .text, // TODO security !!! :) restrict creation of new documents
-                  ),
+              onPressed: () async {
+                String? error = await ProfileHelper.saveBungalow(
+                  ref,
+                  bungalowController.text,
+                );
+
+                if (error != null) {
+                  showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                            title: const Text("Error"),
+                            content: Text(error),
+                          ));
+                }
+              },
               child: const Text("save")),
         ]),
       ),
