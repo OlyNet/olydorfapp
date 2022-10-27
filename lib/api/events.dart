47b4818e08131c13a8f520847dbe57a08cf9e719
Joscha von Andrian <joschavonandrian@gmail.com>
2022-10-27 21:53:40 +0200

    update to new appwrite api

--- lib/api/events.dart
+++ lib/api/events.dart
@@ -1,35 +1,33 @@
 import 'dart:developer';
 import 'dart:typed_data';
-
+import 'package:appwrite/models.dart' as appwriteModels;
 import 'package:appwrite/appwrite.dart';
-import 'package:appwrite/models.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:olydorf/models/event_model.dart';
 
 class EventsState extends StateNotifier<List<Event>> {
   final Client client;
-  late Database database;
+  late Databases databases;
   late Storage storage;
 
   EventsState(this.client) : super([]) {
-    database = Database(client);
+    databases = Databases(client);
     storage = Storage(client);
   }
 
   Future<void> getEvents() async {
     try {
-      final res = await database.listDocuments(collectionId: 'events');
+      final res = await databases.listDocuments(
+          databaseId: 'olydorf', collectionId: 'events');
 
       List<Event> events =
           res.documents.map((doc) => Event.fromMap(doc.data)).toList();
-      log(events.toString());
       for (Event event in events) {
         if (event.imgId != null) {
           event.image = await _getEventImage(event.imgId!);
         }
       }
-      log(events.toString());
       state = events;
     } on AppwriteException catch (e) {
       log(e.message.toString());
@@ -38,7 +36,8 @@ class EventsState extends StateNotifier<List<Event>> {
 
   Future<Uint8List> _getEventImage(String imgId) async {
     try {
-      return await storage.getFilePreview(fileId: imgId);
+      return await storage.getFilePreview(
+          bucketId: 'eventImages', fileId: imgId);
     } on AppwriteException {
       rethrow;
     }
@@ -46,11 +45,13 @@ class EventsState extends StateNotifier<List<Event>> {
 
   Future<String?> uploadEventPicture(XFile file, String imgName) async {
     try {
-      File? result = await storage.createFile(
-        file: MultipartFile.fromBytes('file', await file.readAsBytes(),
-            filename: imgName),
+      appwriteModels.File? result = await storage.createFile(
+        bucketId: 'eventImages',
+        file: InputFile(bytes: await file.readAsBytes(), filename: imgName),
         fileId: 'unique()',
-        read: ['role:all'],
+        permissions: [
+          Permission.read(Role.any()),
+        ],
       );
       return result.$id;
     } catch (e) {
@@ -61,12 +62,13 @@ class EventsState extends StateNotifier<List<Event>> {
 
   Future<void> createEvent(Event event) async {
     try {
-      await database.createDocument(
+      await databases.createDocument(
+          databaseId: 'olydorf',
           collectionId: 'events',
           documentId: "unique()",
           data: event.toMap(),
-          read: [
-            'role:all',
+          permissions: [
+            Permission.read(Role.any()),
           ]);
     } on AppwriteException catch (e) {
       log(e.message.toString());
